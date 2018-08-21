baffadabf82deb8dd395fa4afe793291bc5c4d63
Arvind Kumar <arvind@enkrypt.in>
2018-08-22 04:51:08 +0530

    Improved initial page load animation

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -15,24 +15,73 @@ class Navbar extends React.Component {
             if (this.props.firstLoad) {
                 const sitename = document.getElementById('sitename');
                 const fullname = document.getElementById('fullname');
+                const interimOne = document.getElementById('interim-one');
+                const interimTwo = document.getElementById('interim-two');
+                const interimThree = document.getElementById('interim-three');
                 const navbar = document.getElementById('navbar');
+                const animationSpeed = 0.25; // Change this to control how fast the first page load animation goes
                 new TimelineLite()
-                    .to(sitename, 0.75, {
+                    .to(sitename, animationSpeed, {
                         css: { width: '0' },
-                        delay: 2,
+                        delay: 1,
                         onComplete: () => {
                             sitename.style.display = 'none';
                         },
                         ease: Power2.easeInOut,
                     })
-                    .to(fullname, 0.75, {
+                    .to(interimOne, animationSpeed, {
+                        css: { width: '3em' },
+                        onStart: () => {
+                            interimOne.style.display = 'inline-block';
+                        },
+                        ease: Power2.easeInOut,
+                    })
+                    .to(interimOne, animationSpeed, {
+                        css: { width: '0' },
+                        delay: animationSpeed / 2,
+                        onComplete: () => {
+                            interimOne.style.display = 'none';
+                        },
+                        ease: Power2.easeInOut,
+                    })
+                    .to(interimTwo, animationSpeed, {
+                        css: { width: '3.5em' },
+                        onStart: () => {
+                            interimTwo.style.display = 'inline-block';
+                        },
+                        ease: Power2.easeInOut,
+                    })
+                    .to(interimTwo, animationSpeed, {
+                        css: { width: '0' },
+                        delay: animationSpeed / 2,
+                        onComplete: () => {
+                            interimTwo.style.display = 'none';
+                        },
+                        ease: Power2.easeInOut,
+                    })
+                    .to(interimThree, animationSpeed, {
+                        css: { width: '4.5em' },
+                        onStart: () => {
+                            interimThree.style.display = 'inline-block';
+                        },
+                        ease: Power2.easeInOut,
+                    })
+                    .to(interimThree, animationSpeed, {
+                        css: { width: '0' },
+                        delay: animationSpeed / 2,
+                        onComplete: () => {
+                            interimThree.style.display = 'none';
+                        },
+                        ease: Power2.easeInOut,
+                    })
+                    .to(fullname, animationSpeed, {
                         css: { width: '6em' },
                         onStart: () => {
                             fullname.style.display = 'inline-block';
                         },
                         ease: Power2.easeInOut,
                     })
-                    .to(navbar, 0.75, {
+                    .to(navbar, animationSpeed * 2, {
                         css: { height: '1.23em', paddingTop: '0.25em' },
                         onComplete: () => {
                             this.props.unsetFirstLoad();
@@ -56,6 +105,15 @@ class Navbar extends React.Component {
                         <span id="firstname">arvind </span>
                         <span id="lastname">kumar</span>
                     </a>
+                </div>
+                <div id="interim-three">
+                    <span id="lastname">developer</span>
+                </div>
+                <div id="interim-two">
+                    <span id="firstname">hacker</span>
+                </div>
+                <div id="interim-one">
+                    <span id="lastname">coder</span>
                 </div>
                 <div id="sitename">
                     <span id="firstname">arvind</span>
