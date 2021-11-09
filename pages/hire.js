5d875dbb74136701ed042d67081f4bad707cb823
Arvind Kumar <arvind@enkrypt.in>
2021-11-09 14:36:48 +0530

    Client list on hire page

--- pages/hire.js
+++ pages/hire.js
@@ -3,6 +3,12 @@ import withHydration from '../client/withHydration';
 import HireAvailability from '../components/HireAvailability';
 import Layout from '../components/Layout';
 import { commonExtractor } from '../extractors';
+import crioImage from '../public/images/clients/crio-light.png?resize';
+import geektrustImage from '../public/images/clients/geektrust-light.png?resize';
+import ideogramImage from '../public/images/clients/ideogram-light.png?resize';
+import outreachcircleImage from '../public/images/clients/outreachcircle-light.png?resize';
+import pickcelImage from '../public/images/clients/pickcel-light.png?resize';
+import zyskImage from '../public/images/clients/zysk-light.png?resize';
 import theme from '../theme';
 
 const HydratedHireAvailability = withHydration(
@@ -34,8 +40,7 @@ const Hire = ({ config, fontFaces }) => (
       <Para>
         I have experience developing and deploying diverse tech solutions at
         scale for small, medium and large businesses over the past decade. My
-        proficiency includes software design and architecture in the following
-        domains:
+        proficiency includes software engineering in the following domains:
       </Para>
       <DomainPara>
         <DomainTitle>Frontend</DomainTitle>: Websites that are fast, fluid,
@@ -83,6 +88,78 @@ const Hire = ({ config, fontFaces }) => (
           mail@arvind.io
         </a>
       </Para>
+      <ClientsPara>
+        Some of my past local clients from my own city. I enjoy serving the
+        Bangalore ambition.
+      </ClientsPara>
+      <ClientsGallery>
+        <a href="https://crio.do/" target="_blank" rel="noopener noreferrer">
+          <img
+            alt="Crio"
+            className="lazy"
+            src={crioImage.placeholder}
+            data-srcset={crioImage.srcSet}
+            data-sizes="(max-width: 768px) 220px, 300px"
+          />
+        </a>
+        <a href="https://mallik.in/" target="_blank" rel="noopener noreferrer">
+          <img
+            alt="Ideogram"
+            className="lazy"
+            src={ideogramImage.placeholder}
+            data-srcset={ideogramImage.srcSet}
+            data-sizes="(max-width: 768px) 220px, 300px"
+          />
+        </a>
+        <a href="https://zysk.in/" target="_blank" rel="noopener noreferrer">
+          <img
+            alt="Zysk Technologies"
+            className="lazy"
+            src={zyskImage.placeholder}
+            data-srcset={zyskImage.srcSet}
+            data-sizes="(max-width: 768px) 220px, 300px"
+          />
+        </a>
+        <a
+          href="https://client.outreachcircle.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            alt="OutreachCircle"
+            className="lazy"
+            src={outreachcircleImage.placeholder}
+            data-srcset={outreachcircleImage.srcSet}
+            data-sizes="(max-width: 768px) 220px, 300px"
+          />
+        </a>
+        <a
+          href="https://www.geektrust.in/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            alt="Geektrust"
+            className="lazy"
+            src={geektrustImage.placeholder}
+            data-srcset={geektrustImage.srcSet}
+            data-sizes="(max-width: 768px) 220px, 300px"
+          />
+        </a>
+        <a
+          href="https://www.pickcel.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            alt="Pickcel"
+            className="lazy"
+            src={pickcelImage.placeholder}
+            data-srcset={pickcelImage.srcSet}
+            data-sizes="(max-width: 768px) 220px, 300px"
+          />
+        </a>
+      </ClientsGallery>
     </HirePage>
   </Layout>
 );
@@ -119,6 +196,38 @@ const DomainTitle = styled.div`
   display: inline-block;
   width: 4em;
   color: ${theme.colors.primary};
+`;
+
+const ClientsPara = styled.div`
+  padding: 4em 0 0 0;
+  text-align: center;
+`;
+
+const ClientsGallery = styled.div`
+  display: grid;
+  grid-template-columns: 300px 300px;
+  gap: 2em;
+  margin: 2.5em auto;
+  width: max-content;
+
+  & img {
+    width: 300px;
+    border-radius: 0.2em;
+    filter: grayscale(100%);
+    transition: 0.2s ease;
+  }
+
+  & img:hover {
+    filter: grayscale(0);
+  }
+
+  @media (max-width: 768px) {
+    grid-template-columns: 220px;
+
+    & img {
+      width: 220px;
+    }
+  }
 `;
 
 export const getStaticProps = async (context) => {
